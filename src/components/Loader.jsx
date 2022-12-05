6c88392cdeca9d9f3f23a2463f062178c70caba5
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-05 11:03:47 -0300

    preliminary loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,11 @@
-const Loader = () => (
-  <div>Loader</div>
-);
+import { loader } from "../assets";
 
-export default Loader;
+export default function Loader({ title }) {
+  return (
+    <div className="flex flex-col w-full justify-center items-center">
+      <img src={loader} alt="loader gif" className="w-32 h-32 object-contain" />
+      <h1 className="mt-2 font-bold text-2xl text-gray-100">{title || "Loading..."}</h1>
+    </div>
+  )
+}
+
