636ae3d72eed8bbec5cbb001cd9bce17fb21a608
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-05 11:03:29 -0300

    preliminary error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
 
-const Error = () => (
-  <div>Error</div>
-);
+export default function Error() {
+  return (
+    <div className="flex w-full justify-center items-center">
+      <h1 className="mt-2 font-bold text-2xl text-gray-100">Something went wrong. Please try again.</h1>
+    </div>
+  )
+}
 
-export default Error;
