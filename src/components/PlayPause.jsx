3f61f2da1bde08c6b10e5f9f086310f7156edf81
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-06 19:34:08 -0300

    play-pause feature working right

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,9 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
-export default PlayPause;
+export default function PlayPause({ isPlaying, activeSong, song, handlePause, handlePlay }) {
+    return (
+        (isPlaying && activeSong?.title === song.title) ?
+            <FaPauseCircle size={40} className="text-gray-300" onClick={handlePause} /> :
+            <FaPlayCircle size={40} className="text-gray-300" onClick={handlePlay} />
+    )
+}
