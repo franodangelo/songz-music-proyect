1fe21a7e361724321359e84a75469490317fa513
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-15 15:53:43 -0300

    working on top artists section

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,23 @@
-const TopArtists = () => <div>TopArtists</div>;
+import { ArtistCard, Loader, Error } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
-export default TopArtists;
+export default function TopArtists() {
+
+    const { data, isFetching, error } = useGetTopChartsQuery();
+
+    if (isFetching) return <Loader title="Loading top charts" />;
+    if (error) return <Error />;
+
+    return (
+        <div className="flex flex-col">
+            <h2 className="mt-4 mb-10 font-bold text-3xl text-gray-100 ">Top artists right now</h2>
+            <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+                {data?.map((track) => (
+                    <ArtistCard
+                        key={track.key}
+                        track={track} />
+                ))}
+            </div>
+        </div>
+    )
+}
