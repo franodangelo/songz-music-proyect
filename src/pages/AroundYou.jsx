3e9c23c4387a583e63c261bd664ce3d9211e352e
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-15 15:24:48 -0300

    working on around you section

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { SongCard, Loader, Error } from "../components";
 
-export default CountryTracks;
+export default function AroundYou() {
+
+    const [country, setCountry] = useState("");
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying } = useSelector(state => state.player);
+
+    useEffect(() => {
+        axios.get(`https://geo.ipify.org/api/v1?apiKey=at_MAjsSr22BoweULUpuBx69tv9K1NJV`)
+            .then(res => setCountry(res?.data?.location?.country))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+    }, [country])
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
