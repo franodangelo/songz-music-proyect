6304d3b4909a040e7043753e1f74956740e552e5
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-15 15:50:23 -0300

    top charts section done

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,30 @@
-const TopCharts = () => <div>TopCharts</div>;
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { SongCard, Loader, Error } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
-export default TopCharts;
+export default function TopCharts() {
+
+    const { activeSong, isPlaying } = useSelector(state => state.player);
+    const { data, isFetching, error } = useGetTopChartsQuery();
+
+    if (isFetching) return <Loader title="Loading top charts" />;
+    if (error) return <Error />;
+
+    return (
+        <div className="flex flex-col">
+                <h2 className="mt-4 mb-10 font-bold text-3xl text-gray-100 ">This are the top charts</h2>
+            <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+                {data?.map((song, i) => (
+                    <SongCard
+                        key={song.key}
+                        song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
+                        i={i} />
+                ))}
+            </div>
+        </div>
+    )
+}
