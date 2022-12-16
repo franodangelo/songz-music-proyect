c3bdcc158e64d70221bb2b3ef4c5b8bd33cc4487
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-16 09:54:47 -0300

    working on select for genres

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { SongCard, Loader, Error } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 export default function Discover() {
     const dispatch = useDispatch();
-    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
 
     if (isFetching) return <Loader title={`Loading songs...`} />;
@@ -15,9 +16,9 @@ export default function Discover() {
         <div className="flex flex-col">
             <div className="flex sm:flex-row flex-col w-full mt-4 mb-10 justify-between items-center">
                 <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
-                <select name="" id="" value=""
-                    onChange={() => { }}
-                    className="mt-5 sm:mt-0 p-4 bg-gray-900 text-sm text-gray-300 rounded-lg outline-none">
+                <select name="" id="" value={genreListId || "pop"}
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+                    className="mt-6 sm:mt-0 p-4 bg-gray-900 text-sm text-gray-300 rounded-lg outline-none">
                     {genres.map((genre) => {
                         return <option key={genre.value} value={genre.value}>{genre.title}</option>
                     })}
