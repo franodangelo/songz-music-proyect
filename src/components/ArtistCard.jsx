887dfd6807de65d8a21d285d70e1f50bfdf363ca
Franco D'Angelo <ffrancodangelo@gmail.com>
2022-12-16 09:40:26 -0300

    artist card done

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,18 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
-};
+import { useNavigate } from "react-router-dom";
 
-export default ArtistCard;
+export default function ArtistCard({ track }) {
+
+  const navigate = useNavigate();
+
+  return (
+    <div
+      // onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+      className="flex flex-col w-60 p-4 bg-gray-100/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <img className="w-full h-56 rounded-lg"
+        src={track?.images?.coverart}
+        alt="Artist cover" />
+      <p className="mt-4 font-semibold text-lg text-gray-100 truncate">{track?.subtitle}</p>
+    </div>
+  )
+}
+
