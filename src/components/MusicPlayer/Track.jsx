65181bcaaa2d2a89b55a680b0e3ba3cc9d1fc374
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-09 15:52:42 -0300

    removed animation

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Track = ({ isPlaying, isActive, activeSong }) => (
-  <div className="flex-1 flex items-center justify-start">
-    <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
-      <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
+  <div className="flex-1 flex items-center justify-start gap-4">
+    <div className="hidden sm:block h-16 w-16">
+      <img src={activeSong?.images?.coverart} alt="active song cover art" className="rounded-xl" />
     </div>
     <div className="w-[50%]">
       <p className="truncate text-white font-bold text-lg">
