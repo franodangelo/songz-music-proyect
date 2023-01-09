a679bf0dfdbc49ec3155eb9f1c1e849bb606b387
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-09 16:13:29 -0300

    artist cover instead of coverart

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,3 +1,4 @@
+import { MdImageNotSupported } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 export default function ArtistCard({ track }) {
@@ -8,9 +9,15 @@ export default function ArtistCard({ track }) {
     <div
       // onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
       className="flex flex-col w-60 p-4 bg-gray-100/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <img className="w-full h-56 rounded-lg"
-        src={track?.images?.coverart}
-        alt="Artist cover" />
+      {track?.images?.background ?
+        <img className="w-full h-56 rounded-lg"
+          src={track?.images?.background}
+          alt="Artist cover"
+        /> :
+        <div className="flex flex-col w-full h-56 items-center justify-center rounded-l">
+          <MdImageNotSupported className="w-full h-40 text-gray-500" />
+          <p className="text-sm text-gray-200">Image not found</p>
+        </div>}
       <p className="mt-4 font-semibold text-lg text-gray-100 truncate">{track?.subtitle}</p>
     </div>
   )
