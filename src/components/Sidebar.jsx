ba7c8809ad38990d5fecb3de12917537776ae991
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-11 14:12:13 -0300

    styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const NavLinks = ({ handleClick }) => {
                 onClick={() => handleClick && handleClick()}
                 key={item.name}
                 to={item.to}
-                className="flex items-center font-medium text-sm text-gray-400 hover:text-cyan-400 ease-in-out duration-300">
+                className="flex items-center font-medium text-sm text-gray-400 hover:text-orange-500 ease-in-out duration-300">
                 <item.icon className="w-6 h-6 mr-2" />
                 {item.name}
             </NavLink>
@@ -32,7 +32,7 @@ export default function Sidebar() {
                     <RiCloseLine onClick={() => setMobileMenuOpen(false)} className="w-6 h-6 mr-2 text-gray-100" /> :
                     <HiOutlineMenu onClick={() => setMobileMenuOpen(true)} className="w-6 h-6 mr-2 text-gray-100" />}
             </div>
-            <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-gray-900 backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? "left-0" : "-left-full"}`}>
+            <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-br from-slate-200/10 to-orange-500/10 backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? "left-0" : "-left-full"}`}>
                 <NavLinks handleClick={() => setMobileMenuOpen(false)} />
             </div>
         </>
