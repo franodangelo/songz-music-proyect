4dcb5397e844719b7714e8ac2eaaaa736c66bbb7
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-11 13:48:42 -0300

    new loader with react-loader-spinner

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,17 @@
-import { loader } from "../assets";
+import { Bars } from "react-loader-spinner";
 
 export default function Loader({ title }) {
   return (
     <div className="flex flex-col w-full justify-center items-center">
-      <img src={loader} alt="loader gif" className="w-32 h-32 object-contain" />
+      <Bars
+        height="80"
+        width="80"
+        radius="9"
+        color='#f97316'
+        ariaLabel='three-dots-loading'
+        wrapperStyle
+        wrapperClass
+      />
       <h1 className="mt-2 font-bold text-2xl text-gray-100">{title || "Loading..."}</h1>
     </div>
   )
