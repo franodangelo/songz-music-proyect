57aba280cc5b81680b27754bd9939b6953339466
Franco D'Angelo <ffrancodangelo@gmail.com>
2023-01-13 11:03:19 -0300

    spacing from 2 to 4

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -3,23 +3,23 @@ import { useNavigate } from "react-router-dom";
 
 export default function ArtistCard({ track }) {
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <div
-      // onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
-      className="flex flex-col w-60 p-4 bg-gray-100/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      {track?.images?.background ?
-        <img className="w-full h-56 rounded-lg"
-          src={track?.images?.background}
-          alt="Artist cover"
-        /> :
-        <div className="flex flex-col w-full h-56 items-center justify-center rounded-l">
-          <MdImageNotSupported className="w-full h-40 text-gray-500" />
-          <p className="text-sm text-gray-200">Image not found</p>
-        </div>}
-      <p className="mt-4 font-semibold text-lg text-gray-100 truncate">{track?.subtitle}</p>
-    </div>
-  )
+    return (
+        <div
+            // onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+            className="flex flex-col w-60 p-4 bg-gray-100/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+            {track?.images?.background ?
+                <img className="w-full h-56 rounded-lg"
+                    src={track?.images?.background}
+                    alt="Artist cover"
+                /> :
+                <div className="flex flex-col w-full h-56 items-center justify-center rounded-l">
+                    <MdImageNotSupported className="w-full h-40 text-gray-500" />
+                    <p className="text-sm text-gray-200">Image not found</p>
+                </div>}
+            <p className="mt-4 font-semibold text-lg text-gray-100 truncate">{track?.subtitle}</p>
+        </div>
+    )
 }
 
